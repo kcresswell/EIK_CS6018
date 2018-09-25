f1c171f5213288c83a3fc9c964fcb4628981bd10
iyeung <iyeung@ancestry.com>
2018-09-25 09:25:06 -0600

    Hiking and weather buttons working for mobile, hiking button works for tablet

--- Projects/Project1/app/src/main/java/com/example/mcresswell/project01/HikingActivity.java
+++ Projects/Project1/app/src/main/java/com/example/mcresswell/project01/HikingActivity.java
@@ -1,48 +1,48 @@
-package com.example.mcresswell.project01;
-
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-
-public class HikingActivity extends AppCompatActivity
-        implements HikingFragment.OnHikingFragmentInteractionListener {
-
-    private static final String LOG = HikingActivity.class.getSimpleName();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.d(LOG, "onCreate");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hiking);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        String location = getIntent().getStringExtra("location");
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        //Instantiate a new fragment instance if none exists, otherwise restore previously instantiated instance
-        HikingFragment fragment = savedInstanceState == null ?
-                HikingFragment.newInstance(location) :
-                (HikingFragment) fragmentManager.findFragmentById(R.id.cl_hikes);
-
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        fragmentTransaction.add(R.id.cl_hikes, fragment);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
-
-    }
-
-    @Override
-    public void onHikingFragmentInteraction() {
-
-    }
-}
+//package com.example.mcresswell.project01;
+//
+//import android.net.Uri;
+//import android.os.Bundle;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
+//import android.support.v4.app.FragmentManager;
+//import android.support.v4.app.FragmentTransaction;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.util.Log;
+//import android.view.View;
+//
+//public class HikingActivity extends AppCompatActivity
+//        implements HikingFragment.OnHikingFragmentInteractionListener {
+//
+//    private static final String LOG = HikingActivity.class.getSimpleName();
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        Log.d(LOG, "onCreate");
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_hiking);
+//
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        String location = getIntent().getStringExtra("location");
+//
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        //Instantiate a new fragment instance if none exists, otherwise restore previously instantiated instance
+//        HikingFragment fragment = savedInstanceState == null ?
+//                HikingFragment.newInstance(location) :
+//                (HikingFragment) fragmentManager.findFragmentById(R.id.cl_hikes);
+//
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//
+//        fragmentTransaction.add(R.id.cl_hikes, fragment);
+//        fragmentTransaction.addToBackStack(null);
+//        fragmentTransaction.commit();
+//
+//    }
+//
+//    @Override
+//    public void onHikingFragmentInteraction() {
+//
+//    }
+//}
--- Projects/Project1/app/src/main/java/com/example/mcresswell/project01/HikingFragment.java
+++ Projects/Project1/app/src/main/java/com/example/mcresswell/project01/HikingFragment.java
@@ -1,93 +1,98 @@
-package com.example.mcresswell.project01;
-
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.example.mcresswell.project01.util.Constants;
-
-
-/**
- * Hiking fragment class.
- */
-public class HikingFragment extends Fragment {
-
-    private static final String LOG = HikingFragment.class.getSimpleName();
-
-    private String city;
-    private String country;
-    private OnHikingFragmentInteractionListener mListener;
-
-    public HikingFragment() { }
-
-    public static HikingFragment newInstance(String currentLocation) {
-        Log.d(LOG, Constants.NEW);
-        HikingFragment fragment = new HikingFragment();
-        Bundle args = new Bundle();
-//        args.putString("location", currentLocation);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        Log.d(LOG, Constants.CREATE);
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-//            city = getArguments().getString("city");
-//            country = getArguments().getString("country");
-
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        Log.d(LOG, Constants.CREATE_VIEW);
-
-        View view = inflater.inflate(R.layout.fragment_hiking, container, false);
-        return view;
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        Log.d(LOG, Constants.ATTACH);
-        super.onAttach(context);
-        if (context instanceof OnHikingFragmentInteractionListener) {
-            mListener = (OnHikingFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnHikingFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        Log.d(LOG, Constants.DETACH);
-        super.onDetach();
-        mListener = null;
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle state){
-        super.onSaveInstanceState(state);
-        Log.d(LOG, Constants.SAVE_INSTANCE_STATE);
-
-    }
-
-    /**
-     *  Interface that is implemented by HikingActivity to allow interactions that occur
-     *  in this fragment to be communicated with its hosting activity.
-     **/
-    public interface OnHikingFragmentInteractionListener {
-        void onHikingFragmentInteraction();
-    }
-
-
-}
+//package com.example.mcresswell.project01;
+//
+//import android.content.Context;
+//import android.net.Uri;
+//import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+//import android.util.Log;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//
+//import com.example.mcresswell.project01.util.Constants;
+//
+//
+///**
+// * Hiking fragment class.
+// */
+//public class HikingFragment extends Fragment implements RV_Adapter.OnAdapterDataChannel {
+//
+//    private static final String LOG = HikingFragment.class.getSimpleName();
+//
+//    private String city;
+//    private String country;
+//    private OnHikingFragmentInteractionListener mListener;
+//
+//    public HikingFragment() { }
+//
+//    public static HikingFragment newInstance(String currentLocation) {
+//        Log.d(LOG, Constants.NEW);
+//        HikingFragment fragment = new HikingFragment();
+//        Bundle args = new Bundle();
+////        args.putString("location", currentLocation);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        Log.d(LOG, Constants.CREATE);
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+////            city = getArguments().getString("city");
+////            country = getArguments().getString("country");
+//
+//        }
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        Log.d(LOG, Constants.CREATE_VIEW);
+//
+//        View view = inflater.inflate(R.layout.fragment_hiking, container, false);
+//        return view;
+//    }
+//
+//    @Override
+//    public void onAttach(Context context) {
+//        Log.d(LOG, Constants.ATTACH);
+//        super.onAttach(context);
+//        if (context instanceof OnHikingFragmentInteractionListener) {
+//            mListener = (OnHikingFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnHikingFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        Log.d(LOG, Constants.DETACH);
+//        super.onDetach();
+//        mListener = null;
+//    }
+//
+//    @Override
+//    public void onSaveInstanceState(Bundle state){
+//        super.onSaveInstanceState(state);
+//        Log.d(LOG, Constants.SAVE_INSTANCE_STATE);
+//
+//    }
+//
+//    @Override
+//    public void onAdapterDataPass(int position) {
+//
+//    }
+//
+//    /**
+//     *  Interface that is implemented by HikingActivity to allow interactions that occur
+//     *  in this fragment to be communicated with its hosting activity.
+//     **/
+//    public interface OnHikingFragmentInteractionListener {
+//        void onHikingFragmentInteraction();
+//    }
+//
+//
+//}
