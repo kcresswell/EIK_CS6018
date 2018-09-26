1021e96d85c445d81669d76eb7a0c90aa673e927
iyeung <iyeung@ancestry.com>
2018-09-25 22:35:53 -0600

    Merge branch 'master' into irene
    # Conflicts:
    #	Projects/Project1/app/src/main/java/com/example/mcresswell/project01/DashboardActivity.java
    #	Projects/Project1/app/src/main/java/com/example/mcresswell/project01/FitnessDetailsFragment.java
    #	Projects/Project1/app/src/main/java/com/example/mcresswell/project01/WeatherActivity.java
    #	Projects/Project1/app/src/main/res/layout/fragment_weather.xml

--- Projects/Project1/app/src/main/java/com/example/mcresswell/project01/FitnessDetailsActivity.java
+++ Projects/Project1/app/src/main/java/com/example/mcresswell/project01/FitnessDetailsActivity.java
@@ -1,38 +1,20 @@
 package com.example.mcresswell.project01;
 
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FitnessDetailsActivity extends AppCompatActivity
-    implements FitnessDetailsFragment.onFitnessDetailsInteractionListener {
-
-    public static final double DEFAULT_CALORIES = 2000;
-    public static final double DEFAULT_BASAL_METABOLIC_RATE = 1500;
+public class FitnessDetailsActivity extends AppCompatActivity {
+    private FragmentTransaction m_fTrans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fitness);
 
-        double calsToConsume = getIntent().getDoubleExtra("calories", DEFAULT_CALORIES);
-        double calculatedBmr = getIntent().getDoubleExtra("bmr", DEFAULT_BASAL_METABOLIC_RATE);
-
-        FragmentManager manager = getSupportFragmentManager();
-
-        FitnessDetailsFragment fragment = savedInstanceState == null ?
-              FitnessDetailsFragment.newInstance(calsToConsume, calculatedBmr):
-                (FitnessDetailsFragment) manager.findFragmentById(R.id.fl_master_nd_activity_fitness);
-
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(fragment, null);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-
-    @Override
-    public void onFitnessDetailsFragmentInteraction() {
-
+        //present fragment to display
+        m_fTrans = getSupportFragmentManager().beginTransaction();
+        m_fTrans.replace(R.id.fl_master_nd, new FitnessDetailsFragment(), "v_frag_dashboard");
+        m_fTrans.commit();
     }
 }
