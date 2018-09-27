fe0e8bbc66d9d12a204119b046c2448cb3a82428
Eric <truthdefender02@yahoo.com>
2018-09-26 20:56:16 -0600

    updated button behavior for edit button on ProfileSummaryFragment.

--- Projects/Project1/app/src/main/java/com/example/mcresswell/project01/ProfileDetailsActivity.java
+++ Projects/Project1/app/src/main/java/com/example/mcresswell/project01/ProfileDetailsActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.example.mcresswell.project01.userProfile.UserProfileViewModel;
 
-public class ProfileDetailsActivity extends AppCompatActivity {
+public class ProfileDetailsActivity extends AppCompatActivity implements ProfileSummaryFragment.OnProfileSummaryDataChannel {
     private FragmentTransaction m_fTrans;
     private UserProfileViewModel m_userProfileViewModel;
 
@@ -18,13 +18,27 @@ public class ProfileDetailsActivity extends AppCompatActivity {
 
         initViewModel();
 
-        m_fTrans = getSupportFragmentManager().beginTransaction();
-        m_fTrans.replace(R.id.fl_activity_profile_details, new ProfileSummaryFragment(), "v_frag_profile");
-        m_fTrans.commit();
+        if(savedInstanceState==null) {
+            m_fTrans = getSupportFragmentManager().beginTransaction();
+            m_fTrans.replace(R.id.fl_activity_profile_details, new ProfileSummaryFragment(), "v_frag_profile_summary");
+            m_fTrans.commit();
+        }
     }
 
     private void initViewModel() {
         m_userProfileViewModel = ViewModelProviders.of(this)
                 .get(UserProfileViewModel.class);
+    }
+
+    //collect signal that "edit" button was clicked in the ProfileEntryFragment.
+    @Override
+    public void onProfileSummaryDataPass(boolean isClicked) {
+        if(isClicked){
+            ProfileEntryFragment frag_profileEntry = new ProfileEntryFragment();
+            m_fTrans = getSupportFragmentManager().beginTransaction();
+            m_fTrans.replace(R.id.fl_activity_profile_details, frag_profileEntry, "v_frag_profile_entry");
+            m_fTrans.addToBackStack(null);
+            m_fTrans.commit();
+        }
     }
 }
--- Projects/Project1/app/src/main/java/com/example/mcresswell/project01/ProfileSummaryFragment.java
+++ Projects/Project1/app/src/main/java/com/example/mcresswell/project01/ProfileSummaryFragment.java
@@ -6,13 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfileSummaryFragment extends Fragment {
+public class ProfileSummaryFragment extends Fragment implements View.OnClickListener {
 
+    //declare member variables
+    private Button m_btn_edit;
+    private OnProfileSummaryDataChannel m_dataListener;
 
     public ProfileSummaryFragment() {
         // Required empty public constructor
@@ -22,13 +27,33 @@ public class ProfileSummaryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_summary, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_summary, container, false);
+
+        m_btn_edit = (Button) view.findViewById(R.id.btn_edit);
+        m_btn_edit.setOnClickListener(this);
+
+        //initialize the dataListener to context of activity as a concrete implementation of the interface
+        m_dataListener = (OnProfileSummaryDataChannel) view.getContext();
+        return view;
     }
 
     //edit button on click fragment replace with edit fragment
     //id = btn_edit
+    @Override
     public void onClick(View v) {
-        
+        switch (v.getId()){
+            case R.id.btn_edit: {
+                Toast.makeText(getActivity(), "Edit Button Clicked", Toast.LENGTH_SHORT).show();
+                //send specific data of "TRUE" value signifying the button was clicked
+                m_dataListener.onProfileSummaryDataPass(true);
+
+                break;
+            }
+        }
     }
 
+    //pass data from fragment to activity via interface
+    public interface OnProfileSummaryDataChannel {
+        void onProfileSummaryDataPass(boolean isClicked);
+    }
 }
